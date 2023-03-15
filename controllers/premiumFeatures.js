33bd1c38ed7435f22e5fe562213b5d47ebfc5a60
Chhavi Prabhakar <prabhakarchhavi9@gmail.com>
2023-03-15 22:19:33 +0530

    code optimised

---
+++ controllers/premiumFeatures.js
@@ -0,0 +1,25 @@
+const User = require('../models/user');
+const Expense = require('../models/expense');
+const sequelize = require('../util/database');
+
+exports.getUserLeaderboard = async (req, res, next) => {
+    try {
+        const leaderboardofusers = await User.findAll({
+            attributes: ['id', 'name', [sequelize.fn('sum', sequelize.col('expenses.exAmount')), 'total_cost']],
+            include: [
+                {
+                    model: Expense,
+                    attributes: []
+                }
+            ],
+            group: ['user.id'],
+            order: [['total_cost', 'DESC']]
+
+        })
+
+        res.status(200).json(leaderboardofusers);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err)
+    }
+}
