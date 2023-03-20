2eca95baa7e32110a3f1fee786c742b2944c9457
Chhavi Prabhakar <prabhakarchhavi9@gmail.com>
2023-03-20 14:51:44 +0530

    frontend code

---
+++ views/forgotPswd.js
@@ -0,0 +1,16 @@
+async function forgot(e) {
+    e.preventDefault();
+
+    const email = {
+        email: e.target.email.value
+    }
+
+    try {
+        const response = await axios
+            .post('http://localhost:3000/password/forgotpassword', email);
+            alert(response.data.message);
+    } catch (err) {
+        console.log(JSON.stringify(err));
+        document.body.innerHTML += `<div style="color:red;">${err.message} <div>`;
+    }
+}
