568f9d5bad511d8e7ef5e8454e52bf2724a40b46
Chhavi Prabhakar <prabhakarchhavi9@gmail.com>
2023-03-21 00:00:47 +0530

    app.js and models are here

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const sequelize = require('./util/database');
 const User = require('./models/user');
 const Expense = require('./models/expense');
 const Order = require('./models/order');
+const ForgotPswd = require('./models/forgot');
 
 const app = express();
 
@@ -16,17 +17,22 @@ const userRoutes = require('./routes/user');
 const expenseRoutes = require('./routes/expense');
 const purchaseRoutes = require('./routes/purchase');
 const premiumRoutes = require('./routes/premiumFeatures');
+const forgotRoutes  = require('./routes/forgot');
 
 app.use('/user', userRoutes);
 app.use('/expense', expenseRoutes);
 app.use('/purchase', purchaseRoutes);
 app.use('/premium', premiumRoutes);
+app.use('/password', forgotRoutes);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
 
 User.hasMany(Order);
 Order.belongsTo(User);
+
+User.hasMany(ForgotPswd);
+ForgotPswd.belongsTo(User);
 
 sequelize.sync()  //{force: true}
 .then(result => {
