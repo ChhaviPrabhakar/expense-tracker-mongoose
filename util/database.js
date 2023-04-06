37a74c680c56d7267bed6311355fccf71fa803ed
Chhavi Prabhakar <prabhakarchhavi9@gmail.com>
2023-04-06 17:22:03 +0530

    db

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERID, process.env.DB_PSWD, {
     dialect: 'mysql',
-    host: process.env.DB_HOST
+    host: process.env.DB_HOST || 'localhost'
 });
 
 module.exports = sequelize;
